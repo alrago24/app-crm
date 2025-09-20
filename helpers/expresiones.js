652c6bd3643ee837f0e2ee607b9ac2accf10bca0
alrago24 <alinson_04@hotmail.com>
2025-09-20 14:49:19 -0500

    (develop): Validación de formularios con expresiones regulares

---
+++ helpers/expresiones.js
@@ -0,0 +1,4 @@
+export let expresiones = {
+    texto: /^[a-zA-ZáéíóúÁÉÍÓÚñÑüÜñÑ\s]{5,50}$/,
+    numeros: /^\d{5,10}$/
+}
--- helpers/validarRegistro.js
+++ helpers/validarRegistro.js
@@ -1,15 +1,48 @@
+import { expresiones } from "./expresiones.js";
+
 /*
-querySelectorAll
+querySelectorAll (NodeList)
+getelemtsByClassName (HTMLCollection)
+getelemtsByTagName (HTMLCollection)
 */
 
 /*los getElement nos traen un HTML Collection y los querySelectorAll un NodeList*/
 let inputs = document.querySelectorAll('.formulario__entrada'); /*en estos casos no se puede usar métodos de array*/
 /*Se debe convertir a arreglo*/
 let arregloInputs = [...inputs]; //Convertir a un arreglo - expansion iterable
-console.log(inputs);
-
 
 arregloInputs.map((input)=>{
-    console.log(arregloInputs);
+    input.addEventListener("keyup", (e) =>{
+        switch (e.target.id) {
+            case "nombre":
+                if (expresiones.texto.test(e.target.value)) {
+                    e.target.style.borderColor = "green"
+                    e.target.style.color = "green"
+                } else {
+                    document.getElementById("nombre").style.borderColor = "red"
+                    document.getElementById("nombre").style.color = "red"
+                }
+                break;
+            case "apellido":
+                if (expresiones.texto.test(e.target.value)) {
+                    e.target.classList.add("correcto")
+                    e.target.classList.remove("incorrecto")
+                } else {
+                    e.target.classList.add("incorrecto")
+                    e.target.classList.remove("correcto")
+                }
+            break;
+            case "documento":
+                if (expresiones.numeros.test(e.target.value)) {
+                    e.target.classList.add("correcto")
+                    e.target.classList.remove("incorrecto")
+                } else {
+                    e.target.classList.add("incorrecto")
+                    e.target.classList.remove("correcto")
+                }
+            break
+        }
+        
+    })
 })
 
